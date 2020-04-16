793dec25d98dceab9dd34750cec001336c2a5f48
3Eleven <1131141128@qq.com>
2020-04-16 19:33:06 +0800

    mido: Force enable mobile network settings v2

--- prop.mk
+++ prop.mk
@@ -174,7 +174,9 @@ rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,22 \
-service.qti.ims.enabled=1
+service.qti.ims.enabled=1 \
+persist.vendor.radio.flexmap_type=none \
+persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
